18c8ce8e5f0b9ec74c48e68a10b13e77bb3a0ef5
Andrew <lazerabate9119@gmail.com>
2022-05-09 19:10:13 +0300

    init: add file js masiv languages and utilits

---
+++ js/language/import.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/extensions */
+import ru from './ru.js'; // Импорт русской клавы
+import en from './en.js'; // Импорт русской клавы
+
+export default { ru, en }; // Собираем в один общий объект(Экспорт наружу)
